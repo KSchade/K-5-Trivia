293bbce03131892121fb9a1e699d8bb5e43291a7
Kellie Schade <kellieschade@gmail.com>
2018-06-07 09:22:10 -0400

    slideshow

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #projectOne
+The K-5th trivia game is a graded mini quiz to test your basic knowledge of elementary subjects. 
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 function check(){
     var question1 = document.quiz.questionOne.value;
     var question2 = document.quiz.questionTwo.value;
@@ -7,10 +6,12 @@ function check(){
     var question4 = document.quiz.questionFour.value;
     var question5 = document.quiz.questionFive.value;
     var question6 = document.quiz.questionSix.value;
-var correct=0;
-    if( question1=="Chlorophyll") {
-        correct++;
-    }
+
+    var correct=0;
+
+if( question1=="Chlorophyll") {
+    correct++;
+}
 
 if ( question2 == "Michigan") {
     correct++;
@@ -27,12 +28,8 @@ if (question5 == "Bagheera") {
 if (question6 == "3") {
     correct++;
 }
-//    document.getElementById("after_submit").style.visability = 'visible';
-    // document.getElementById("number_correct").innerHTML = "(You got " + correct + " out of 6 correct!)";
-    // console.log(number_correct)
 
-
-var messages=["Awesome job!", "Not bad:)", "Mehhhh..."];
+var messages=["Awesome job!", "Not bad :)", "Mehhhh..."];
 var score;
 if(correct<=2) {
     score=2;
@@ -46,5 +43,54 @@ if (correct>4 && correct<=6) {
 
 document.getElementById('message').innerHTML = messages[score];
 document.getElementById("number_correct").innerHTML = "(You got " + correct + " out of 6 correct!)";
-console.log(number_correct)
+console.log(number_correct);
+
 }
+var sliderQuestions = document.querySelectorAll('.slide'),
+arrowLeft = document.querySelector('#arrow-left'),
+arrowRight = document.querySelector('#arrow-right'),
+current = 0;
+//clear all images
+function reset(){
+    for(let i = 0; i < sliderQuestions.length; i++) {
+        sliderQuestions[i].style.display = 'none';
+    }
+}
+//just shows the first question
+function startSlide(){
+    reset();
+
+    sliderQuestions[0].style.display = 'block';
+}
+
+
+//show previous slide
+function slideLeft (){
+    reset();
+    sliderQuestions[current - 1].style.display = 'block';
+    current --;
+}
+//show next slide
+function slideRight() {
+    reset();
+    sliderQuestions[current + 1].style.display = 'block';
+    current++;
+}
+//right arrow click event
+arrowRight.addEventListener('click', function(){
+    if (current === sliderQuestions.length - 1) {
+        current = -1
+    }
+    slideRight();
+});
+
+//add event listener so the arrows click left
+arrowLeft.addEventListener('click', function() {
+    if (current === 0){
+        current = sliderQuestions.length;
+    }
+    slideLeft();
+});
+startSlide();
+
+
