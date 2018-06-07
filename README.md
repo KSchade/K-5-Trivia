c597e61863d8635a2e05fbc6d539bad93d8d4148
Kellie Schade <kellieschade@gmail.com>
2018-06-07 18:23:13 -0400

    added arrows on first and last slide and read me

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-#projectOne
-The K-5th trivia game is a graded mini quiz to test your basic knowledge of elementary subjects. 
+#projectOne K-5 Trivia Game
+K-5 trivia is a one player game that tracks each selected answer and displays the users final score,dependent on the users input, after clicking submit.
+
+
+
+
+
+## You Do
+
+Complete all six questions and click submit when finished. 
+
+
+
+## Goals
+- Each question is presented one at a time.-- slide show.
+- When an answer is selected keep track of the response.
+- If an question is left blank user should not get credit. 
+- After clicking submit the user should see their final score.
+- Have a message display after user clicks submit.
+- Have 3 different messages dependent on the users score 1-2, 3-4, 5-6.
+- When code is refreshed all questions are reset.
+- HTML, CSS and JavaScript should all pass validators.
+
+## Pseudocode
+//score
+// hard code 6 questions in html
+// If user selects a correct answer add a point to the total
+// If user selects an incorrect answer do not add a point to the total.
+//after answering all questions user clicks submit button
+//Out of 6 questions how many answers did the user get correct should display.
+// if the user gets 1 or 2 correct display that message
+// if the user gets 3 or 4 correct display that message
+// if the user gets 5 or 6 correct display that message
+
+
+
+//slideshow
+//When page is visited the first question should display
+//First slide should only be able to select next.
+// When user clicks next the following slide should appear
+// previous and next arrows should appear on slides 2-5
+// on last slide only a previous arrow should appear.
+// If the page is not refreshed selected answers should retain if user clicks previous.
+// User should only see slides, after game has completed they should not see the background. 
+//If page is refreshed all answers are cleared. 
+
+
--- script.js
+++ script.js
@@ -1,96 +1,111 @@
+function check() {
+  var question1 = document.quiz.questionOne.value;
+  var question2 = document.quiz.questionTwo.value;
+  var question3 = document.quiz.questionThree.value;
+  var question4 = document.quiz.questionFour.value;
+  var question5 = document.quiz.questionFive.value;
+  var question6 = document.quiz.questionSix.value;
 
-function check(){
-    var question1 = document.quiz.questionOne.value;
-    var question2 = document.quiz.questionTwo.value;
-    var question3 = document.quiz.questionThree.value;
-    var question4 = document.quiz.questionFour.value;
-    var question5 = document.quiz.questionFive.value;
-    var question6 = document.quiz.questionSix.value;
+  var correct = 0;
 
-    var correct=0;
-
-if( question1=="Chlorophyll") {
+  if (question1 == "Chlorophyll") {
     correct++;
-}
+  }
 
-if ( question2 == "Michigan") {
+  if (question2 == "Michigan") {
     correct++;
-}
-if (question3 =="northCarolina") {
+  }
+  if (question3 == "northCarolina") {
     correct++;
-}
-if (question4 == "6"){
+  }
+  if (question4 == "6") {
     correct++;
-}
-if (question5 == "Bagheera") {
+  }
+  if (question5 == "Bagheera") {
     correct++;
-}
-if (question6 == "3") {
+  }
+  if (question6 == "3") {
     correct++;
-}
+  }
 
-var messages=["Awesome job!", "Not bad :)", "Mehhhh..."];
-var score;
-if(correct<=2) {
-    score=2;
-}
-if(correct>2 && correct<=4) {
-    score=1;
-}
-if (correct>4 && correct<=6) {
-    score= 0;
-}
+  var messages = ["Awesome job!", "Not bad :)", "Mehhhh..."];
+  var score;
+  if (correct <= 2) {
+    score = 2;
+  }
+  if (correct > 2 && correct <= 4) {
+    score = 1;
+  }
+  if (correct > 4 && correct <= 6) {
+    score = 0;
+  }
 
-document.getElementById('message').innerHTML = messages[score];
-document.getElementById("number_correct").innerHTML = "(You got " + correct + " out of 6 correct!)";
-console.log(number_correct);
-
+  document.getElementById("message").innerHTML = messages[score];
+  document.getElementById("number_correct").innerHTML =
+    "(You got " + correct + " out of 6 correct!)";
+  console.log(number_correct);
 }
-var sliderQuestions = document.querySelectorAll('.slide'),
-arrowLeft = document.querySelector('#arrow-left'),
-arrowRight = document.querySelector('#arrow-right'),
-current = 0;
+var sliderQuestions = document.querySelectorAll(".slide"),
+  arrowLeft = document.querySelector("#arrow-left"),
+  arrowRight = document.querySelector("#arrow-right"),
+  current = 0;
 //clear all images
-function reset(){
-    for(let i = 0; i < sliderQuestions.length; i++) {
-        sliderQuestions[i].style.display = 'none';
-    }
-}
-//just shows the first question
-function startSlide(){
-    reset();
-
-    sliderQuestions[0].style.display = 'block';
+function reset() {
+  for (let i = 0; i < sliderQuestions.length; i++) {
+    sliderQuestions[i].style.display = "none";
+  }
 }
 
+var slideOne = document.querySelector(".slide1");
+var slideSix = document.querySelector(".slide6");
+arrowLeft = document.querySelector("#arrow-left");
+arrowRight = document.querySelector("#arrow-right");
+var sliderQuestions = document.querySelectorAll(".slide"),
+  current = 0;
+
+//just shows the first question  _________
+function startSlide() {
+  reset();
+  arrowLeft.style.display = "none";
+  sliderQuestions[0].style.display = "block";
+  
+}
 
 //show previous slide
-function slideLeft (){
-    reset();
-    sliderQuestions[current - 1].style.display = 'block';
-    current --;
+function slideLeft() {
+  reset();
+  arrowLeft.style.display = "block";
+  arrowRight.style.display = "block";
+  sliderQuestions[current - 1].style.display = "block";
+  current--;
 }
 //show next slide
 function slideRight() {
-    reset();
-    sliderQuestions[current + 1].style.display = 'block';
-    current++;
+  reset();
+  arrowLeft.style.display = "block";
+  sliderQuestions[current + 1].style.display = "block";
+  current++;
 }
 //right arrow click event
-arrowRight.addEventListener('click', function(){
-    if (current === sliderQuestions.length - 1) {
-        current = -1
-    }
-    slideRight();
+arrowRight.addEventListener("click", function() {
+  if (current === sliderQuestions.length - 1) {
+    current = -1;
+    console.log("fire")
+  
+    
+  }
+  if(current === sliderQuestions.length -2){
+  arrowRight.style.display= "none";
+  }
+  slideRight();
 });
 
 //add event listener so the arrows click left
-arrowLeft.addEventListener('click', function() {
-    if (current === 0){
-        current = sliderQuestions.length;
-    }
-    slideLeft();
+arrowLeft.addEventListener("click", function() {
+  if (current === 0) {
+    current = sliderQuestions.length;
+  }
+  slideLeft();
 });
 startSlide();
-
 
